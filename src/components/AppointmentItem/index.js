edefee02c1f8e96f9cb631b2ef0ff2228566395d
Bhargav-9121 <bkc.edu9121@gmail.com>
2023-10-29 08:36:22 +0000

    done

---
+++ src/components/AppointmentItem/index.js
@@ -0,0 +1,29 @@
+import './index.css'
+import {format, parseISO} from 'date-fns'
+
+const AppointmentItem = props => {
+  const {total, togStar} = props
+  const {id, tit, dateThing, starred} = total
+  const starUrl = starred
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  const doThis = () => {
+    togStar(id)
+  }
+
+  const parsedDate = parseISO(dateThing)
+  const formattedDate = format(parsedDate, 'dd MMMM yyyy, EEEE')
+
+  return (
+    <li>
+      <p>{tit}</p>
+      <p>{`Date: ${formattedDate}`}</p>
+      <button type="button" data-testid="star">
+        <img onClick={doThis} src={starUrl} alt="star" />
+      </button>
+    </li>
+  )
+}
+
+export default AppointmentItem
---
+++ src/components/Appointments/index.js
@@ -0,0 +1,108 @@
+import {Component} from 'react'
+import './index.css'
+import {v4} from 'uuid'
+import AppointmentItem from '../AppointmentItem'
+
+class Appointments extends Component {
+  state = {
+    appoiList: [],
+    titleInput: '',
+    dateInput: '',
+    filteredStatus: false,
+  }
+
+  titChange = event => {
+    this.setState({titleInput: event.target.value})
+  }
+
+  dateChange = event => {
+    this.setState({dateInput: event.target.value})
+  }
+
+  submitForm = event => {
+    event.preventDefault()
+
+    const {titleInput, dateInput} = this.state
+
+    const newAppoi = {
+      id: v4(),
+      tit: titleInput,
+      dateThing: dateInput,
+      starred: false,
+    }
+    this.setState(prevState => ({
+      appoiList: [...prevState.appoiList, newAppoi],
+      titleInput: '',
+      dateInput: '',
+    }))
+  }
+
+  togStar = id => {
+    this.setState(prevState => ({
+      appoiList: prevState.appoiList.map(each => {
+        if (each.id === id) {
+          return {...each, starred: !each.starred}
+        }
+        return each
+      }),
+    }))
+  }
+
+  filterStarred = () => {
+    this.setState(prevState => ({filteredStatus: !prevState.filteredStatus}))
+  }
+
+  render() {
+    const {appoiList, titleInput, dateInput, filteredStatus} = this.state
+
+    const filteredAppoi = filteredStatus
+      ? appoiList.filter(each => each.starred === true)
+      : appoiList
+
+    return (
+      <div>
+        <div>
+          <h1>Add Appointment</h1>
+
+          <form onSubmit={this.submitForm}>
+            <label htmlFor="tit-in">TITLE</label>
+            <input
+              value={titleInput}
+              onChange={this.titChange}
+              type="text"
+              id="tit-in"
+            />
+
+            <label htmlFor="date-in">DATE</label>
+            <input
+              value={dateInput}
+              onChange={this.dateChange}
+              type="date"
+              id="date-in"
+            />
+
+            <button type="submit">Add</button>
+          </form>
+
+          <hr />
+
+          <h1>Appointments</h1>
+          <button onClick={this.filterStarred} type="button">
+            Starred
+          </button>
+          <ul>
+            {filteredAppoi.map(eachAppoi => (
+              <AppointmentItem
+                togStar={this.togStar}
+                key={eachAppoi.id}
+                total={eachAppoi}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Appointments
